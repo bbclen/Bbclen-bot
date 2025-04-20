5df641b92de343a0e25bb6acb212825bec62a702
bbclen <bbclenfromrichard@gmail.com>
2025-04-20 13:46:13 +0100

    Update session.js

--- session.js
+++ session.js
@@ -40,10 +40,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~CBFhgZxT#b0rmikctx9vGU8pO_BwcjBeY5-dVQdyxM0UB2-C52Aw",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
-    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Bbclen bot",
+    SUDO: process.env.SUDO ? process.env.SUDO.split(".") : ["237675556199"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "session",
     FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
